28e3f619d7c70792ae2dded464996027473058fc
Larissa Noella Shimirwa <s.noella@alustudent.com>
2025-07-25 14:20:23 +0200

    Delete src directory

--- src/js/superbaseClient.js
+++
@@ -1,7 +0,0 @@
-
-
-const supabaseUrl = 'https://mllcxecflmjwzbamhjzq.supabase.co'; // replace this
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sbGN4ZWNmbG1qd3piYW1oanpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5ODY4NzMsImV4cCI6MjA2NzU2Mjg3M30.16d6W6nYx9G-fP9dY0iPQcQs6TBPqzswSZJSEix4ZW4'; // replace this
-
-const { createClient } = supabase;
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
